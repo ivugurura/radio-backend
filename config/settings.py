52e9aeae0c77d0853f621ca2e2e80a1a41847f6b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-07 20:59:50 +0200

    Set up app folders

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     # Third-party
     "graphene_django",
     "django_filters",
+		# Apps
+		"apps.media",
+		"apps.studio",
+		"apps.users",
 ]
 
 MIDDLEWARE = [
