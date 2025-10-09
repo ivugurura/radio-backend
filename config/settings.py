b558661741bf629a7c3219bb6675212210246401
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-09 15:27:12 +0200

    Fix relation references

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = "users.User"
+
 GRAPHENE = {
     "SCHEMA": "config.schema.schema",
     "MIDDLEWARE": [
