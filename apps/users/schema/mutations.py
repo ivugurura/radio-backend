35cdd686a5b4d7d83726336a6eb0906321a933e8
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-15 15:18:01 +0200

    Fix login

--- apps/users/schema/mutations.py
+++ apps/users/schema/mutations.py
@@ -1,7 +1,9 @@
 import graphene
 import graphql_jwt
 from django.contrib.auth import authenticate
+from django.db.models import Q
 from graphql_jwt.shortcuts import get_token
+from rest_framework.authtoken.models import Token
 
 from apps.users.models import User
 from apps.users.types.user_types import UserType
@@ -9,29 +11,26 @@ from apps.users.types.user_types import UserType
 
 class RegisterUser(graphene.Mutation):
     class Arguments:
-        username = graphene.String(required=True)
+        user_name = graphene.String(required=True)
         password = graphene.String(required=True)
         email = graphene.String(required=True)
+        first_name = graphene.String(required=True)
+        last_name = graphene.String(required=True)
         display_name = graphene.String(required=False)
+        phone = graphene.String(required=False)
+        profile_picture = graphene.String(required=False)
 
     user = graphene.Field(UserType)
     token = graphene.String()
-    refresh_token = graphene.String()
 
-    def mutate(self, info, username, password, email, display_name=None):
-        if User.objects.filter(username=username).exists():
+    def mutate(self, info, user_name, email, **kwargs):
+        if User.objects.filter(Q(user_name=user_name) | Q(email=email)).exists():
             raise Exception("Username already taken")
 
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            email=email,
-            display_name=display_name or username,
-        )
+        user = User.objects.create_user(user_name=user_name, email=email, **kwargs)
         # Immediately issue tokens (optional)
         payload = get_token(user)
-        refresh = graphql_jwt.shortcuts.create_refresh_token(user)
-        return RegisterUser(user=user, token=payload, refresh_token=refresh)
+        return RegisterUser(user=user, token=payload)
 
 
 class CustomLogin(graphene.Mutation):
@@ -41,15 +40,16 @@ class CustomLogin(graphene.Mutation):
 
     user = graphene.Field(UserType)
     token = graphene.String()
-    refresh_token = graphene.String()
+    rest_token = graphene.String()
 
     def mutate(self, info, username, password):
         user = authenticate(username=username, password=password)
         if not user:
             raise Exception("Invalid credentials")
         token = graphql_jwt.shortcuts.get_token(user)
-        refresh_token = graphql_jwt.shortcuts.create_refresh_token(user)
-        return CustomLogin(user=user, token=token, refresh_token=refresh_token)
+        rest_payload = Token.objects.get_or_create(user=user)
+
+        return CustomLogin(user=user, token=token, rest_token=rest_payload[0])
 
 
 class UserMutations(graphene.ObjectType):
