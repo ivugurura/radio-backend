01c43dca788a9a68500a37554acc5b1278441cf5
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-15 15:17:44 +0200

    Configure restframework library

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     # Third-party
     "graphene_django",
     "django_filters",
+    'rest_framework',
+	'rest_framework.authtoken',
     # Apps
     "apps.users",
     "apps.studio",
