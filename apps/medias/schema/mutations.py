5b27c83ab654419ec92dd70337916a6ac077e06e
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-10-29 18:03:34 +0200

    Implement delete

--- apps/medias/schema/mutations.py
+++ apps/medias/schema/mutations.py
@@ -5,6 +5,7 @@ from django.db import transaction
 from graphql_jwt.decorators import login_required
 
 from apps.medias.models import UploadSession, Track
+from apps.medias.services.delete import delete_track_files
 from apps.medias.services.upload import init_upload, ensure_upload_token, finalize_upload
 from apps.studio.models import Studio
 from apps.medias.tasks import start_pipeline_for_upload
@@ -84,6 +85,47 @@ class FinalizeUpload(graphene.Mutation):
         return FinalizeUpload(ok=True, track_id=track.id)
 
 
+class DeleteTrack(graphene.Mutation):
+    class Arguments:
+        track_id = graphene.UUID(required=True)
+
+    ok = graphene.Boolean()
+
+    @classmethod
+    @transaction.atomic
+    @login_required
+    def mutate(cls, root, info, track_id):
+        user = info.context.user
+        if not user or not user.is_authenticated:
+            raise Exception("auth required")
+
+        try:
+            track = Track.objects.select_related("studio", "upload_session").get(id=track_id)
+        except Track.DoesNotExist:
+            # idempotent OK (already deleted)
+            return DeleteTrack(ok=True)
+
+        # TODO: permission check (user can manage track in this studio)
+
+        # Remove files first (best effort)
+        delete_track_files(track)
+
+        # Optionally delete the related UploadSession to clean up database (if you prefer to keep history, remove this)
+        up = track.upload_session
+        track.delete()
+
+        if up:
+            # If no other tracks refer to this session, delete it
+            if not UploadSession.objects.filter(id=up.id).exists():
+                try:
+                    up.delete()
+                except Exception:
+                    pass
+
+        return DeleteTrack(ok=True)
+
+
 class MediasMutations(graphene.ObjectType):
     request_upload = RequestUpload.Field()
     finalize_upload = FinalizeUpload.Field()
+    delete_track = DeleteTrack.Field()
