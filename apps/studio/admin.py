cec40a6ddb1551196e497d87ef56d833b64f10fa
Akimana A Jean d Amour <akimanaja17@gmail.com>
2026-08-28 12:59:40 +0200

    Add admin side

--- apps/studio/admin.py
+++ apps/studio/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.studio.models import Studio, StreamingCredential
+
+
+@admin.register(Studio)
+class StudioAdmin(admin.ModelAdmin):
+    list_display = ("display_name", "slug", "is_active", "default_br_kbps")
+    search_fields = ("display_name", "slug")
+
+
+@admin.register(StreamingCredential)
+class StreamingCredentialAdmin(admin.ModelAdmin):
+    list_display = ("studio", "username", "rotated_at")
+    readonly_fields = ("password_encrypted", "rotated_at")
+    search_fields = ("studio__slug", "username")
